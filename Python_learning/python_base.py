e4c874cff7d896a0964caef3895494f086bf2ebc
Leo-wang-han <wanghan001516@gmail.com>
2026-02-23 18:47:52 +0800

    增加一些以后学习的文件

---
+++ Python_learning/python_base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   python_base.py
+@Time    :   2026/02/23 18:44:54
+@Author  :   wanghan 
+@Email   :   wanghan001516@163.com
+@description   :   xxxxxxxxx
+'''
